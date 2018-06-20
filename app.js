d1fa76cbda8ad8f89f9f04d62a07726c8cdba67c
Qasim Hussain <syd.qasimhussain@gmail.com>
2018-06-20 17:30:11 +0200

    adding comments and documentation.

--- app.js
+++ app.js
@@ -19,17 +19,26 @@ app.use(logger());
 
 var tree = new Tree();
 
-// then prepare the data using 
+// Initializing tree with the JSON data of POIs
 for (var i = 0; i < jsonContent.length; i++){
 	tree.set(calculateHash(jsonContent[i].lng,jsonContent[i].lat),jsonContent[i],12);
 }
 
+/**
+ * Displays all possible POIs.
+ */
 app.use(route.get('/all', (ctx, next) => {
   ctx.body = {
     jsonContent,
   };
 }));
 
+/**
+ * Searches the provided with those of POIs.
+ * @param {string} lng - The latitude of provided location.
+ * @param {string} lat - The longitude of provided location.
+ * @param {string} proximity - The proximity area of search.
+ */
 app.use(route.get('/search', (ctx, next) => {
 	var srcLocationHash = calculateHash(ctx.query.lng, ctx.query.lat,ctx.query.proximity); 
 	var result = matchPrefix(srcLocationHash);
@@ -42,17 +51,26 @@ app.use(route.get('/search', (ctx, next) => {
 
 }));
 
+/**
+ * Matches the prefixes of Geohashes of location Provided with those of POIs.
+ * @param {string} srcLocationHash - The Geohash of provided location.
+ */
 function matchPrefix(srcLocationHash) { 
 	
    return tree.get(srcLocationHash); 
 } 
 
+/**
+ * Calculates the Geohashes of Location Params.
+ * @param {string} lng - The longitude of the Location .
+ * @param {string} lat - The latitude of the Location.
+ * @param {string} proximity - The proximity of the Location.
+ */
 function calculateHash(lng,lat,proximity) { 
 	var hash = Geohash.encode(lng,lat,proximity);
 	return hash; 
 } 
 
-// Serve static files
 app.use(serve(path.join(__dirname, 'public')));
 app.use(compress());
 
